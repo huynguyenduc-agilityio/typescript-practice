05825bceb1779be4917ea5a0e16e3fc35dbef429
Huy Duc Nguyen <huy.nguyenduc@asnet.com.vn>
2023-08-13 23:10:29 +0700

    Fix server

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./src/server/index.json')
+const router = jsonServer.router('src/server/index.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
