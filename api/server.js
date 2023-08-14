6d77244dee1041e5196f06c53362ab4029cea946
Huy Nguyen Duc <huy.nguyenduc@asnet.com.vn>
2023-08-14 10:40:05 +0700

    Fix server CORs

--- api/server.js
+++ api/server.js
@@ -1,17 +1,34 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('src/server/index.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('src/server/index.json');
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 3000;
 
-server.use(middlewares)
+server.use(middlewares);
+
+// Custom middleware to add CORS headers
+server.use((req, res, next) => {
+    // Allow requests from any origin for development
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Specify the allowed HTTP methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    // Specify the allowed headers
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // Allow credentials (if needed)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+}));
+
+server.use(router);
+
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
